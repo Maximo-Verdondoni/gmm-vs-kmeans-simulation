7772d56c06d35ad5595260a41a39e1a25b0ff286
Maximo <avryux04@gmail.com>
2026-05-27 00:52:58 -0300

    feat(simulation,py): se agrego el 5to escenario, la metrica silhouette y criterios de corte para GMM

--- src/simulation.py
+++ src/simulation.py
@@ -3,14 +3,15 @@ from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
 from sklearn.metrics import adjusted_rand_score
 from src.data_generation import generar_datos_escenario
+from sklearn.metrics import silhouette_score
 
 def ejecutar_montecarlo(M_replicas=200):
     """
-    Ejecuta M réplicas de Montecarlo sobre los 4 escenarios y calcula el ARI.
+    Ejecuta M réplicas de Montecarlo sobre los 5 escenarios y calcula el ARI.
     """
     resultados = []
     
-    for escenario in [1, 2, 3, 4]:
+    for escenario in [1, 2, 3, 4, 5]:
         for m in range(M_replicas):
             # 1. Generar nueva muestra
             X, y_real = generar_datos_escenario(escenario_id=escenario)
@@ -21,7 +22,8 @@ def ejecutar_montecarlo(M_replicas=200):
             ari_km = adjusted_rand_score(y_real, y_pred_km)
             
             # 3. Ajustar GMM
-            gmm = GaussianMixture(n_components=3, covariance_type='full', random_state=m)
+            gmm = GaussianMixture(n_components=3, covariance_type='full', 
+                random_state=m, reg_covar=1e-4, max_iter=200)
             y_pred_gmm = gmm.fit_predict(X)
             ari_gmm = adjusted_rand_score(y_real, y_pred_gmm)
             
@@ -30,13 +32,15 @@ def ejecutar_montecarlo(M_replicas=200):
                 'Escenario': escenario,
                 'Replica': m,
                 'Modelo': 'K-Means',
-                'ARI': ari_km
+                'ARI': ari_km,
+                'Silhouette': silhouette_score(X, y_pred_km)
             })
             resultados.append({
                 'Escenario': escenario,
                 'Replica': m,
                 'Modelo': 'GMM',
-                'ARI': ari_gmm
+                'ARI': ari_gmm,
+                'Silhouette': silhouette_score(X, y_pred_gmm)
             })
             
     return pd.DataFrame(resultados)
